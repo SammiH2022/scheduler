9d84dca7925e5f5e655be558288f5ef21fde35ca
Sammi Huang <sammi9070@Sammis-MacBook-Pro.local>
2022-01-10 11:51:01 -0600

    Jan10

--- src/App.js
+++ src/App.js
@@ -63,15 +63,21 @@ const toggle = (x, lst) => (
 
 const CourseList = ({ courses }) => {
   const [term, setTerm] = useState('Fall');
+  const [selected, setSelected] = useState([]);
   const termCourses = Object.values(courses).filter(course => term === getCourseTerm(course));
   
   return (
     <>
       <TermSelector term={term} setTerm={setTerm} />
       <div className="course-list">
-      { termCourses.map(course => <Course key={course.id} course={ course } />) }
+      { 
+        termCourses.map(course =>
+          <Course key={ course.id } course={ course }
+            selected={selected} setSelected={ setSelected } 
+          />) 
+      }
       </div>
-   </>
+    </>
   );
 };
 
